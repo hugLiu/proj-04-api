eb7d7d5c26eee1975397b40ceb81dced6c560eda
lxk <lliu606@hotmail.com>
2018-02-08 14:41:02 +0800

    api

---
+++ code/GTAPI/GTAPI.Oracle.DataBase/OracleAccess.cs
@@ -0,0 +1,35 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTAPI.Oracle.DataBase
+{
+    public class OracleAccess
+    {
+        public static string ConnectOracle()
+        {
+            try
+            {
+                string connString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.1.239)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORCL)));Persist Security Info=True;User ID=xj_gtapi;Password=gtapi;";
+                // "Data Source= (DESCRIPTION =     (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.1.115)(PORT = 1521))     (CONNECT_DATA =       (SERVER = DEDICATED)       (SERVICE_NAME = ORCL)     )   );User ID=PMDB;Password=officialsite;Persist Security Info=True" providerName="Oracle.ManagedDataAccess.Client" 
+                OracleConnection conn = new OracleConnection(connString);
+                var sql = "select ID from DEP_DEPARTMENT"; 
+                OracleCommand cmd = new OracleCommand(); 
+                cmd.Connection = conn;
+                conn.Open(); 
+                cmd.CommandText = sql; 
+                Object ret = cmd.ExecuteScalar();
+
+
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ code/GTAPI/GTAPI.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GTAPI.Oracle.DataBase;
+
+namespace GTAPI.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = 1;
+            string t = OracleAccess.ConnectOracle();
+        }
+    }
+}
